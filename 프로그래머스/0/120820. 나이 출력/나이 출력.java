abf09219b0e28b2931dc45941101c521e3102d6a
kyungtae43 <129928604+ravenstorm43@users.noreply.github.com>
2024-04-25 11:47:11 +0900

    Update 나이 출력.java

--- 프로그래머스/0/120820. 나이 출력/나이 출력.java
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int age) {
-        int answer = 2022 - age + 1;
+        int answer = 2022 - age + 1; // 0살이 아닌 1살부터 시작하기 때문에 1을 더해줌
         return answer;
     }
 }
