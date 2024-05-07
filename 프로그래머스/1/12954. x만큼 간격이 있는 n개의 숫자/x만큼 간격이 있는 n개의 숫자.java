f7ce201ec8f1a746c2c06a09bf3de2ea5bd6ecce
kyungtae43 <129928604+ravenstorm43@users.noreply.github.com>
2024-05-07 09:17:14 +0900

    Update x만큼 간격이 있는 n개의 숫자.java

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long[] solution(long x, int n) {
+    public long[] solution(long x, int n) { // x는 -10000000 이상, 10000000 이하인 정수여서 long형으로 선언
         long[] answer = new long[n];
         for(int i = 0; i < n; i++) {
             answer[i] = x * (i + 1);
