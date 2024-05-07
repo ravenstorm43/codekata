1c92bebf4c3d7861659edd1bd65835c0ff2e7f0b
kyungtae43 <129928604+ravenstorm43@users.noreply.github.com>
2024-05-07 09:29:58 +0900

    Update 경기도에 위치한 식품창고 목록 출력하기.sql

--- 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
+++ 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -1,3 +1,4 @@
 -- 코드를 입력하세요
 SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS, IFNULL(FREEZER_YN, "N") FROM FOOD_WAREHOUSE
 WHERE ADDRESS LIKE "경기도%"
+// IFNULL(필드명, 대체할 값) : 레코드의 특정 필드의 값이 NULL일때 다른 값으로 대체하는 함수
