629ba36315760b1f4330339502e42d9c332edf95
kyungtae43 <129928604+ravenstorm43@users.noreply.github.com>
2024-05-08 09:54:10 +0900

    Update 자연수 뒤집어 배열로 만들기.java

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -3,7 +3,7 @@ class Solution {
         String n1 = String.valueOf(n);
         int[] answer = new int[n1.length()];
         for(int i = 0; i < n1.length(); i++) {
-            answer[i] = Character.getNumericValue(n1.charAt(n1.length() - i - 1));
+            answer[i] = Character.getNumericValue(n1.charAt(n1.length() - i - 1)); // Character.getNumericValue : 문자 -> 숫자로 변환
         }
         return answer;
     }
