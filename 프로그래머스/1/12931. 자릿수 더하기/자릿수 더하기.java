25eac0d25f287c3d12437028aa35c9e4db662726
kyungtae43 <129928604+ravenstorm43@users.noreply.github.com>
2024-05-09 17:26:26 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -5,7 +5,7 @@ public class Solution {
         int answer = 0;
         String temp = String.valueOf(n);
         for(int i = 0; i < temp.length(); i++) {
-            answer += Character.getNumericValue(temp.charAt(i));    
+            answer += Character.getNumericValue(temp.charAt(i));    // Character.getNumericValue():매개변수로 받은 문자를 정수로 바꾸는 메소드
         }
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("Hello Java");
